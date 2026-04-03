9407c782894f72ca302950630c890bcf2c38b26c
Đỗ An Bình <binhdath06811@gmail.com>
2026-04-03 21:09:05 +0700

    remove unnecessary line

--- src/main/java/vn/fpoly/project/service/UserService.java
+++ src/main/java/vn/fpoly/project/service/UserService.java
@@ -45,13 +45,11 @@ public class UserService {
         if (newPassword.equals(confirmPassword)) {
         user us = repo.findByPhone(phone);
         if (us == null) {
-            System.out.println("doi mat khau that bai");
             return false;
         }
         else {
             us.setPassword(newPassword);
             repo.save(us);
-            System.out.println("doi mat khau thanhc ong");
             return true;
         }
         }
